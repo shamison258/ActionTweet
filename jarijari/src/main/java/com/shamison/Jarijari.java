026da0b91a2b4607e286c3b475670d3e9452e543
sham <sham@192.168.111.100>
2014-07-31 00:41:41 +0900

    Oauth認証を実装。

--- jarijari/src/main/java/com/shamison/Jarijari.java
+++ jarijari/src/main/java/com/shamison/Jarijari.java
@@ -3,10 +3,14 @@ package com.shamison;
 import java.util.Calendar;
 
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import twitter4j.Status;
@@ -15,54 +19,77 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
-public class Jarijari extends JavaPlugin implements Listener{
+public class Jarijari extends JavaPlugin implements Listener, CommandExecutor{
 
+	private String pin;
 	private Twitter twitter;
-	private String ACCESSTOKEN = null;
-	private String ACCESSSECRET = null;
-	private String CONSUMERKEY = null;
-	private String CONSUMERSECRET = null;
+	private String CONSUMERKEY;
+	private String CONSUMERSECRET;
+	private String ACCESSSECRET;
+	private String ACCESSTOKEN;
+	private AccessToken accessToken;
+	private RequestToken requestToken;
+
 
 
 	public void onEnable(){
 		getLogger().info("enable");
 		getServer().getPluginManager().registerEvents(this, this);
+		this.saveDefaultConfig();
+		this.setupTwitterUtil();
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		if(cmd.getName().equalsIgnoreCase("oauth")){
+				try {
+					requestToken = twitter.getOAuthRequestToken();
+					requestToken.getToken();
+					requestToken.getTokenSecret();
+					if (null == accessToken) {
+						sender.sendMessage("clickURL: "+ requestToken.getAuthorizationURL());
+						sender.sendMessage("/pin [pin] plz");
+					}else{
+						sender.sendMessage("don't need oauth.");
+					}
+				} catch (TwitterException e) {
+					e.printStackTrace();
+				}
+			return true;
+		}else if(cmd.getName().equalsIgnoreCase("pin")){
+			this.pin = args[0];
+			try {
+				accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+				this.getConfig().set("ACCESSTOKEN", accessToken.getToken());
+				this.getConfig().set("ACCESSECRET", accessToken.getTokenSecret());
+				this.saveConfig();
+				sender.sendMessage("sucess");
+			} catch (TwitterException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			return true;
+		}
+			return false;
+
+	}
+
+	public void setupTwitterUtil() {
 		TwitterFactory tf = new TwitterFactory();
 		twitter = tf.getInstance();
-		CONSUMERKEY = "xxx";
-		CONSUMERSECRET = "xxx";
-		ACCESSTOKEN = "xxx";
-		ACCESSSECRET = "xxx";
+		CONSUMERKEY = "Z6LpQ2jsdD8neVoCxVyVg";
+		CONSUMERSECRET = "UcI6T9DqsLrpgobfztOZ3WRR4xuLjHAj4nxAE8iG8";
+		ACCESSTOKEN = this.getConfig().getString("ACCESSTOKEN");
+		ACCESSSECRET = this.getConfig().getString("ACCESSSECRET");
 		twitter.setOAuthConsumer(CONSUMERKEY, CONSUMERSECRET);
-		twitter.setOAuthAccessToken(new AccessToken(ACCESSTOKEN, ACCESSSECRET));
-	}
 
-	public void onDisable(){
-		getLogger().info("disable");
-	}
-
-	@EventHandler
-	public void onBlockBreak(BlockBreakEvent event){
-		Material block = event.getBlock().getType();
-		if(block == Material.GRAVEL){
-			Player player = event.getPlayer();
-			this.Tweet("あぁ＾〜"+ player.getName() +"のこころじゃりじゃりするんじゃあ＾〜 [" + this.time() + "]");
+		if(null == ACCESSTOKEN || null == ACCESSSECRET){
+			return;
 		}
-	}
+		twitter.setOAuthAccessToken(new AccessToken(ACCESSTOKEN, ACCESSSECRET));
 
-	public String time(){
-		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH) + 1;
-		int day = cal.get(Calendar.DATE);
-		int hour = cal.get(Calendar.HOUR_OF_DAY);
-		int minute = cal.get(Calendar.MINUTE);
-		int second = cal.get(Calendar.SECOND);
-		String time = year + "/" + month + "/" + day +  " " + hour + ":" + minute + ":" + second ;
-		return time;
 	}
-
 
 	public void Tweet(String tweet){
 		StatusUpdate update = new StatusUpdate(tweet);
@@ -75,4 +102,36 @@ public class Jarijari extends JavaPlugin implements Listener{
 		}
 	}
 
+	public void onDisable(){
+		getLogger().info("disable");
+	}
+
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event){
+		Material block = event.getBlock().getType();
+		if(block == Material.GRAVEL){
+			Player player = event.getPlayer();
+			this.Tweet("あぁ＾〜"+ player.getName() +"のこころじゃりじゃりするんじゃあ＾〜 [" + this.time() + "]");
+		}
+	}
+
+	@EventHandler
+	public void onDeathLava(PlayerDeathEvent event){
+		if(event.getDeathMessage().contains("lava")){
+			event.setDeathMessage("溶岩の中、あったかいナリィ・・・");
+			this.Tweet("溶岩の中、あったかいナリィ・・・ [" + this.time() + "]");
+		}
+	}
+	public String time(){
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DATE);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		String time = year + "/" + month + "/" + day +  " " + hour + ":" + minute + ":" + second ;
+		return time;
+	}
 }
